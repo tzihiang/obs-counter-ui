6751f138c3e341028695995ca7d718b63113c695
Willis Tay <willistzh1996@hotmail.sg>
2024-08-05 21:43:49 +0800

    Added Link Routing

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,9 +1,14 @@
-import { Container, Typography } from "@mui/material"
+import { Button, Container, Stack, Typography } from "@mui/material"
+import { Link } from "react-router-dom";
 
 const Root = (): JSX.Element => {
   return (
     <Container>
       <Typography>Welcome to the main app!</Typography>
+      <Stack rowGap={2}>
+        <Link to="/lifeCounter/1"><Button variant="contained">Go to Life Counter 1</Button></Link>
+        <Link to="/lifeCounter/2"><Button variant="contained">Go to Life Counter 2</Button></Link>
+      </Stack>
     </Container>
   )
 }
